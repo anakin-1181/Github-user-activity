718627c53f7eb2d324611a8eca0dc4c13b15ba77
Anakin Lai <anakin252lai@gmail.com>
2025-05-28 08:44:24 +0100

    event types update

--- app.py
+++ app.py
@@ -2,16 +2,41 @@ from fetch_data import import_data
 
 def showMessage(username):
     response = import_data(username)
+    count = 0
     for event in response:
         match event["type"]:
             case "PushEvent":
                 print(f"- Pushed {len(event["payload"]["commits"])} commits to {event["repo"]["name"]}")
             case "PullRequestEvent":
-                print(f"- {event["payload"]["action"]} a pull request in {event["repo"]["name"]}")
+                action = event["payload"]["action"]
+                print(f"- {action[0].upper()}{action[1::]} a pull request in {event["repo"]["name"]}")
             case "WatchEvent":
                 print(f"- Starred {event["repo"]["name"]}")
+            case "CreateEvent":
+                print(f"- Created {event["payload"]["ref_type"]} in {event["repo"]["name"]}")
+            case "DeleteEvent":
+                print(f"- Deleted {event["payload"]["ref_type"]} in {event["repo"]["name"]}")
+            case "ForkEvent":
+                print(f"- Forked {event["repo"]["name"]}")
+            case "IssueCommentEvent":
+                action = event["payload"]["action"]
+                print(f"- {action[0].upper()}{action[1::]} a comment in {event["repo"]["name"]}")
+            case "IssuesEvent":
+                action = event["payload"]["action"]
+                print(f"- {action[0].upper()}{action[1::]} an issue in {event["repo"]["name"]}")
+            case "PublicEvent":
+                print(f"- Turned {event["repo"]["name"]} from private to public")
+            case "PullRequestReviewEvent":
+                action = event["payload"]["action"]
+                print(f"- {action[0].upper()}{action[1::]} a pull request review in {event["repo"]["name"]}")
+            case "PullRequestReviewCommentEvent":
+                action = event["payload"]["action"]
+                print(f"- {action[0].upper()}{action[1::]} a pull request review comment in {event["repo"]["name"]}")
+        count += 1  
+    print(f"\nResults: {count}/30 events shown")    
                 
+                       
 if __name__ == "__main__":
-    username = input("Please provide a github username.")
+    username = input("Please provide a github username: ")
     showMessage(username)
     
